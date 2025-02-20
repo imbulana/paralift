49a3b7e1e8dab66d62b36c07ffb073d80de4c00b
imbulana <damithckumara@gmail.com>
2025-02-20 02:27:58 -0500

    add skeleton

--- lift.py
+++ lift.py
@@ -1,2 +1,18 @@
 import numpy as np
 import pyvista as pv
+
+# receive bb (singular)
+
+# hough filter (only ellipses for now)
+
+# transform to center
+
+# find minimal peak path to center (origin)
+
+# reflect path around origin
+
+# fit sinusoidal to path (solve sys of eqns.) for symmetric surfaces
+# or Taylor approx./interpolation for complex surfaces
+
+def lift():
+    pass
